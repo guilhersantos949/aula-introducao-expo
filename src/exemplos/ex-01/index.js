c7258a1fa21e946b889efa46241188ea1826da97
guilhersantos949 <guilhermemans@outlook.com>
2025-02-26 20:40:59 -0300

    aula-introdução-componete

---
+++ src/exemplos/ex-01/index.js
@@ -0,0 +1,12 @@
+import { View,Text } from 'react-native';
+import styles from './styles';
+
+function Exemplo01 () {
+    return(
+        <View style = {styles.container}>
+            <Text style= {{color:'#ffff'}}>Exemplo - 01</Text>
+        </View>
+    )
+    
+}
+    export default Exemplo01;
